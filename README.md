01a8a159d263ed73480292f93413da0f6950b613
danwakeem <danwakeem@gmail.com>
2021-04-03 08:39:46 -0500

    v2.18.2

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Status
 ✅ - Working
 
+> Seems to not capture every request though 🤔
+
 # Serverless GQL Demo
 This is a super bare bones AWS Lambda graphql server. I put this together to demo some issues I was having with apollo's `ApolloServerPluginUsageReporting` plugin not working an a lambda environment.
 
--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ const { APOLLO_ENGINE_API_KEY: key, STAGE: graphVariant } = process.env;
 
 const server = new ApolloServer({
   schema: buildFederatedSchema([healthCheckSchema]),
-  engine: {
-    apiKey: key,
-    schemaTag: graphVariant,
-    privateHeaders: ['x-api-key'],
-    sendReportsImmediately: true,
-  },
+  apollo: { key, graphVariant },
+  plugins: [
+    ApolloServerPluginUsageReporting({
+      sendReportsImmediately: true,
+      sendHeaders: { exceptNames: ['x-api-key'] },
+      sendVariableValues: { all: true },
+    }),
+  ],
   introspection: true,
 });
 
